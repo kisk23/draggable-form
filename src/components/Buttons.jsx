08535f0e560c71b25df7320e185f646ff6a73a7d
kisk <YOUR-CORRECT-GITHUB-EMAIL@example.com>
2025-09-23 19:39:21 +0300

    test deplyment

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -37,7 +37,7 @@ const Button = ({ variant, size, children }) => {
   }
 
   return   <DraggableField id={`button-${variant}-${size}-${Math.random().toString(36).substr(2, 9)}`}>
-    <Component style={styles}>{children}</Component> </DraggableField>;
+    <Component style={styles }>{children} </Component> </DraggableField>;
 };
 
 const ButtonBase = styled.button`
